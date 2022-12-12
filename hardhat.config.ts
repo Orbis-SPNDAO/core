be12e1644ac37b182a933b8142184df39aa9ec3c
Brian Grosso <bgro63@gmail.com>
2022-12-12 16:02:10 -0500

    init

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,24 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+require("@nomicfoundation/hardhat-toolbox")
+require("hardhat-deploy")
+require("hardhat-deploy-ethers")
+require("./tasks")
+require("dotenv").config()
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.17",
-};
-
-export default config;
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const MUMBAI_ALCHEMY_URL = process.env.MUMBAI_ALCHEMY_URL
+console.log({ MUMBAI_ALCHEMY_URL })
+module.exports = {
+    solidity: "0.8.17",
+    networks: {
+        mumbai: {
+            url: MUMBAI_ALCHEMY_URL,
+            accounts: [PRIVATE_KEY],
+        },
+    },
+    paths: {
+        sources: "./contracts",
+        tests: "./test",
+        cache: "./cache",
+        artifacts: "./artifacts",
+    },
+}
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,23 +1,29 @@
-import { ethers } from "hardhat";
+const { ethers } = require("hardhat");
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+const deploy = async () => {
+  try {
+    const contractFactorySBT = await ethers.getContractFactory(
+      "SpendSBT"
+    );
+    const sbtContract = await contractFactorySBT.deploy();
+    await sbtContract.deployed();
 
-  const lockedAmount = ethers.utils.parseEther("1");
+    console.log("SBT Contract deployed to:", sbtContract.address);
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    const contractFactoryAdmin = await ethers.getContractFactory(
+      "SpendAdmin"
+    );
+    const adminContract = await contractFactoryAdmin.deploy();
+    await adminContract.deployed();
 
-  await lock.deployed();
+    console.log("Admin Contract deployed to:", adminContract.address);
 
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
-}
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+  
+deploy();
