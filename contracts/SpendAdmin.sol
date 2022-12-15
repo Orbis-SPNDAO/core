606d8be7fc8b27006c48b3a731686a478cfaaa34
Brian Grosso <bgro63@gmail.com>
2022-12-14 19:29:52 -0500

    spendsbt update to shorten ownerToTokenIds on burn

--- contracts/SpendAdmin.sol
+++ contracts/SpendAdmin.sol
@@ -4,52 +4,38 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract SpendSBT is
-    ERC721,
-    ERC721Enumerable,
-    ERC721URIStorage,
-    ERC721Burnable,
-    Ownable
-{
+contract SpendAdmin is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, Ownable, ERC721Burnable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
-    mapping(address => uint256[]) public ownerToTokenIds;
-    
-    string[] public cidList;
-    
-    constructor() ERC721("Spend DAO", "SPN") {
-        safeMint(msg.sender, '');
+    constructor() ERC721("SpendAdmin", "SPTA") {
+        safeMint(msg.sender, "");
     }
 
     function _baseURI() internal pure override returns (string memory) {
-        return "https://patterndao.mypinata.cloud/ipfs/";
-        
+        return "https://ccdao.mypinata.cloud/ipfs/";
     }
 
-    function safeMint(address to, string memory uri) public {
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
-
-        _mint(to, tokenId); // safeMint is not currently supported by on FVM
+        
+        _mint(to, tokenId);
         _setTokenURI(tokenId, uri);
-
-        ownerToTokenIds[to].push(tokenId);
-        cidList.push(uri);
-    }
-
-    function totalSupply() public view override returns (uint256) {
-        return _tokenIdCounter.current();
-    }
-
-    function userBurn(uint id) public {
-        require(ownerOf(id) == msg.sender, "You do not own this token");
-        _burn(id);
     }
 
     // The following functions are overrides required by Solidity.
@@ -58,19 +44,19 @@ contract SpendSBT is
         address to,
         uint256 /*tokenId*/,
         uint256 /*batchSize*/
-    ) internal pure override(ERC721, ERC721Enumerable) {
+    ) internal whenNotPaused override(ERC721, ERC721Enumerable) {
         require(
             from == address(0) || to == address(0),
             "This a Soulbound token. It cannot be transferred. It can only be burned by the token owner."
         );        
     }
-
-    function _burn(uint256 tokenId)
-        internal
-        override(ERC721, ERC721URIStorage)
-    {        
-        // delete ownerToTokenIds[msg.sender][tokenId];
+    
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
         super._burn(tokenId);
+    }
+
+    function totalSupply() public view override returns (uint256) {
+        return _tokenIdCounter.current();
     }
 
     function tokenURI(uint256 tokenId)
