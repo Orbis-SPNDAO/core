824fd69db010fba88117fec314c95bd20870f75a
Brian Grosso <bgro63@gmail.com>
2022-12-30 22:24:48 -0500

    verify

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,12 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-ethers";
 require('dotenv').config()
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY!
 const MUMBAI_ALCHEMY_URL = process.env.MUMBAI_ALCHEMY_URL
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const POLYGON_SCAN_API_KEY = process.env.POLYGON_SCAN_API_KEY
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
@@ -14,7 +16,7 @@ const config: HardhatUserConfig = {
     },
   },
   etherscan: {
-    apiKey: ETHERSCAN_API_KEY
+    apiKey: POLYGON_SCAN_API_KEY
   }
 };
 
